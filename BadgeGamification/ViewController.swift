8fec46ed34acb293abce944818c8c7f2133df627
Bianca Yoshie Itiroko <itirokob@icloud.com>
2018-01-08 15:45:33 -0200

    Initial Commit

---
+++ BadgeGamification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BadgeGamification
+//
+//  Created by Bianca Itiroko on 08/01/18.
+//  Copyright © 2018 Bianca Itiroko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
